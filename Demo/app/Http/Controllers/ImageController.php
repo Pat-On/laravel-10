2102c213a697fedb7557a4b20b0c63055743d0ec
Patryk <patryk.r.nowak@gmail.com>
2023-10-19 05:58:32 +0100

    uploading files part 2

--- Demo/app/Http/Controllers/ImageController.php
+++ Demo/app/Http/Controllers/ImageController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 class ImageController extends Controller
 {
     //
-    public function handleImage (){
-        return "hello";
+    public function handleImage(Request $request)
+    {
+        // return $request->all(); // return entire request back
+
+        // dd($request->file('image'));
+        // `/` as a default storage/app/public
+        $request->image->storeAs('/images', 'new_image.jpg');
     }
 }

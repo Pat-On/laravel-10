f79573a1fa12d0437f46d1a1819eac148448820f
Patryk <patryk.r.nowak@gmail.com>
2023-10-22 13:46:29 +0100

    migrations and models

--- Demo/app/Models/Post.php
+++ Demo/app/Models/Post.php
@@ -4,45 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
-
-// php artisan make:model Post
-// must follow singular naming convention
-// then laravel know to which table it should be connected
 class Post extends Model
 {
     use HasFactory;
-    
-    // implementing soft delete
-    use SoftDeletes;
-
-    // You can redefine to what table model belongs example
-    // protected $table = 'posts';
-
-    // fillable - for mass assignments
-    protected $fillable = [
-        'title',
-        // 'status',
-        'description',
-        // 'published',
-        'category_id',
-        'image'
-    ];
-
-    // guarded  - if empty - you can do mass assignment on everything
-    // protected $guarded = [
-    // 'title'
-    // ];
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
-
-
-    public function tags()
-    {
-        return $this->belongsToMany(Tag::class, 'post_tag', );
-    }
 }
--- Demo/database/migrations/2023_10_22_124208_create_categories_table.php
+++ Demo/database/migrations/2023_10_22_124208_create_categories_table.php
@@ -8,12 +8,10 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
@@ -22,11 +20,9 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('categories');
     }
 };

c8cc83daeff5a9e2e131e79ca00b8df77ec9921b
Patryk <patryk.r.nowak@gmail.com>
2023-12-05 07:14:59 +0000

    76 create

---
+++ Demo/app/Http/Controllers/PostController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use File;
+use Illuminate\Support\Facades\Cache;
+
+class PostController extends Controller
+{
+    public function __construct()
+    {
+        // $this->middleware('authCheck2')->only(['create', 'show']);
+        // $this->middleware('authCheck2')->except('index');
+
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       
+
+        return view('index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        return view('create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'image' => ['required', 'max:2028', 'image'],
+            'title' => ['required', 'max: 255'],
+            'category_id' => ['required', 'integer'],
+            'description' => ['required']
+        ]);
+
+        dd('success');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+
+    }
+
+    public function trashed()
+    {
+
+    }
+
+    public function restore($id)
+    {
+
+    }
+
+    public function forceDelete($id)
+    {
+
+    }
+}

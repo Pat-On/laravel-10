ecb63ed4efc9eecf8a2a59cdffbc091469a9f795
Patryk <patryk.r.nowak@gmail.com>
2023-12-24 12:48:17 +0000

    attaching files to an email

--- Demo/app/Mail/OrderShipped.php
+++ Demo/app/Mail/OrderShipped.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -51,6 +52,8 @@ class OrderShipped extends Mailable
      */
     public function attachments(): array
     {
-        return [];
+        return [
+            Attachment::fromPath('/storage/uploads/file_test.jpg')
+        ];
     }
 }

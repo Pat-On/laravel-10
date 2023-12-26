989e985192e1f4233a328d1f3d42aadac1c15dd1
Patryk <patryk.r.nowak@gmail.com>
2023-12-26 14:02:51 +0000

    redeclared routes

--- Demo_2/routes/web.php
+++ Demo_2/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,12 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::get('/posts/trash', [PostController::class, 'trashed'])->name('posts.trashed');
+
+Route::get('/posts/{id}/restore', [PostController::class, 'restore'])->name('posts.restore');
+
+Route::delete('/posts/{id}/force-delete', [PostController::class, 'forceDelete'])->name('posts.force_delete');
+
+Route::resource('posts', PostController::class);
